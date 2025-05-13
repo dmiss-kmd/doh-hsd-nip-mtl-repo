f7c8b0c45621bc31acad222b11cfedb67c98b52e
Jonathan Figueroa <jdfigueroa@doh.gov.ph>
2025-05-13 07:34:28 +0000

    commit all changes

---
+++ definitions/data_sources.js
@@ -0,0 +1,11 @@
+  declare({
+    database: "doh-hsd",
+    schema: "nip_mtl_source",
+    name: "tbl_chd_inv_staging",
+  });
+
+  declare({
+    database: "doh-hsd",
+    schema: "nip_mtl_source",
+    name: "tbl_prov_inv_staging",
+  });
---
+++ includes/tbldef.js
@@ -0,0 +1,33 @@
+const extract_date = `Extract Date`;
+const region_psgc = `Region PSGC`;
+const region = `Region Name`;
+const provhuc_psgc = `Province HUC PSGC`;
+const prov_huc = `Province HUC Name`;
+const warehousecode = `Warehouse Code`;
+const warehousename = `Warehouse Name`;
+const productcode = `Product Code`;
+const vaxtype = `Vaccine Type`;
+const productdesc = `Product Description`;
+const lotno = `Lot Number`;
+const expiry_date = `Expiry Date`;
+const balanceqty = `Remaining quantity`;
+const qtyindoses = `Remaining quantity in doses`;
+const rowhash   = `md1 hash of all data fields concatenated`;
+
+module.exports = {
+    extract_date,
+    region_psgc,
+    region,
+    provhuc_psgc,
+    prov_huc,
+    warehousecode,
+    warehousename,
+    productcode,
+    vaxtype,
+    productdesc,
+    lotno,
+    expiry_date,
+    balanceqty,
+    qtyindoses,
+    rowhash
+};

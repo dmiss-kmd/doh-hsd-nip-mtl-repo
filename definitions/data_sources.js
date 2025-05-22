f0befaa04b5da120ae14205e6e3352385ca64351
Jonathan Figueroa <jdfigueroa@doh.gov.ph>
2025-05-22 07:25:36 +0000

    Commit changes to code

--- definitions/data_sources.js
+++ definitions/data_sources.js
@@ -9,3 +9,15 @@
     schema: "nip_mtl_staging",
     name: "tbl_prov_inv_staging",
   });
+
+  declare({
+    database: "doh-hsd",
+    schema: "nip_mtl_staging",
+    name: "tbl_prov_annual_req",
+  });
+
+  declare({
+    database: "doh-hsd",
+    schema: "nip_mtl_staging",
+    name: "tbl_chd_annual_req",
+  });
